9acbf5d1c59adffb53cc4ecab3df6d3dc7fa3016
Denis13s <141165300+Denis13s@users.noreply.github.com>
2023-11-01 19:11:08 +0100

    Initial commit

---
+++ DeliveryApp/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  DeliveryApp
+//
+//  Created by Denis Yarets on 01/11/2023.
+//
+
+import Foundation
---
+++ DeliveryApp/Services/DataService.swift
@@ -0,0 +1,12 @@
+//
+//  DataService.swift
+//  DeliveryApp
+//
+//  Created by Denis Yarets on 01/11/2023.
+//
+
+import Foundation
+
+final class DataService {
+    
+}
