f96a87c93948d6f559ddf2b98a2404b6be771dd3
Denis13s <141165300+Denis13s@users.noreply.github.com>
2023-11-03 20:12:06 +0100

    Add imaged, DataService, Models, DataService initialization inside TabBarController

--- DeliveryApp/Services/DataService.swift
+++ DeliveryApp/Services/DataService.swift
@@ -9,4 +9,16 @@ import Foundation
 
 final class DataService {
     
+    static let shared = DataService()
+    
+    let categories: [Category]
+    let restaurants: [Restaurant]
+    let menuItems: [MenuItem]
+    
+    private init() {
+        categories = Category.getData()
+        restaurants = Restaurant.getData()
+        menuItems = MenuItem.getData()
+    }
+    
 }
